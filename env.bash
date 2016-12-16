8ee81f982ec0da0279da497fed37200595c41147
Dave Roe <droe@nmdp.org>
2016-12-16 11:33:06 -0600

    Changed the 2DL4 reference to the one in KP420446 (https://www.ncbi.nlm.nih.gov/nuccore/KP420446). It is 2000 bp longer than before: 1000 on each end of the gene.

--- env.bash
+++ env.bash
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 # e.g., jar -cvf kharsh_0.1.jar org; cp kharsh.groovy kharsh_0.1.jar ~/git/kharsh/src/
 
-export PATH=$PATH:$PWD/src
-export PATH=$PATH:$PWD/src/jvarkit/
+export PATH=$PWD/src:$PATH
+export PATH=$PWD/src/jvarkit/:$PATH
 export CLASSPATH=$PWD/src/jvarkit/sam2tsv.jar:$CLASSPATH
-#export CLASSPATH=$PWD/src/kharsh_0.1.jar:$CLASSPATH
-export CLASSPATH=$PWD:$CLASSPATH
+export CLASSPATH=$PWD/src/kharsh_0.1.jar:$CLASSPATH
 export CLASSPATH=$PWD/src/CsvFileReader_2.1.jar:$CLASSPATH
 export CLASSPATH=$PWD/src/dsh-commandline-1.1.jar:$CLASSPATH
 export CLASSPATH=$PWD/src/guava-11.0.2.jar:$CLASSPATH
